794f0cbec99eca5d1e111926a31deff6ca18f403
Pito Salas <pitosalas@gmail.com>
2018-08-17 08:02:44 -0700

    minor

--- scripts/message_publisher.py
+++ scripts/message_publisher.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 import rospy
-from basics.msg import Complex from random import random
+from rosbook.msg import Complex 
+from random import random
+
 rospy.init_node('message_publisher')
-pub = rospy.Publisher('complex', Complex)
+pub = rospy.Publisher('complex', Complex, queue_size=1)
 rate = rospy.Rate(2)
-while not rospy.is_shutdown(): msg = Complex()
+while not rospy.is_shutdown(): 
+    msg = Complex()
     msg.real = random()
     msg.imaginary = random()
     pub.publish(msg)
---
+++ scripts/message_subscriber.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import rospy
+from rosbook.msg import Complex
+
+def callback(msg):
+    print 'Real:', msg.real
+    print 'Imaginary:', msg.imaginary
+    print
+    
+rospy.init_node('message_subscriber')
+sub = rospy.Subscriber('complex', Complex, callback)
+rospy.spin()
