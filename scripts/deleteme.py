e5e058f0b6161f749dac29cb6aab3a34e199e76f
Pito Salas <pitosalas@gmail.com>
2018-08-21 18:53:15 -0400

    bugs

---
+++ scripts/deleteme.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+import rospy
+rospy.init_node('h')
+print(rospy.Time.now())
--- scripts/red_light_green_light.py
+++ scripts/red_light_green_light.py
@@ -2,27 +2,28 @@
 import rospy
 from geometry_msgs.msg import Twist
 
-cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 rospy.init_node('red_light_green_light')
 
 red_light_twist = Twist()
 green_light_twist = Twist()
-green_light_twist.linear.x = 0.5
+green_light_twist.linear.x = 2.5
 
 driving_forward = False
 light_change_time = rospy.Time.now()
+print (rospy.Time.now())
 rate = rospy.Rate(10)
 
 while not rospy.is_shutdown():
+    print(light_change_time, rospy.Time.now(), light_change_time < rospy.Time.now())
     if driving_forward:
-        print("forward")
+        print("green", green_light_twist.linear.x)
         cmd_vel_pub.publish(green_light_twist)
     else:
-        print("stop")
+        print("red")
         cmd_vel_pub.publish(red_light_twist)
-    print(light_change_time)
-    if light_change_time > rospy.Time.now():
-        print("reverse")
+    if light_change_time < rospy.Time.now():
+        print("flip")
         driving_forward = not driving_forward
         light_change_time = rospy.Time.now() + rospy.Duration(3)
     rate.sleep()
