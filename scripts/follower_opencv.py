76366a9303a0d926f46bee109562112514d7eef5
Pito Salas <pitosalas@gmail.com>
2018-09-24 11:10:36 -0400

    small

--- scripts/follower_opencv.py
+++ scripts/follower_opencv.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+
 import rospy
 import cv2
+import sys
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -17,21 +19,29 @@ class Follower:
     def image_callback(self, frame):
         # Convert image to cv2 and display it
         try:
-            self.cv_img = self.bridge.imgmsg_to_cv2(frame, "bgr8")
-        except CvBridgeError as exc:
-            print(traceback.format_exc())
+            print 21
+            self.cv_img = self.bridge.imgmsg_to_cv2(frame, desired_encoding="bgr8")
+            print 23
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
 
 def main():
     cv2.namedWindow('Original')
+    print 30
     follower = Follower()
     rospy.init_node('follower', anonymous=True)
 
     while(not rospy.is_shutdown()):
+        print 35
         if follower.cv_img is not None:
+            print 37
             cv2.imshow('Original', follower.cv_img)
+            print 39
         key = cv2.waitKey(3) & 0xFF
         if key == 27:
             break
+        print 43
         rospy.spin()
 
 if __name__ == '__main__':
