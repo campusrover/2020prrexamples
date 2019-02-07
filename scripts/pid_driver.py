cebaf21fe8b85143b63f559c4cb1b8dbb306b0ca
Pito Salas <pitosalas@gmail.com>
2019-02-07 14:55:02 -0500

    more

--- scripts/pid_driver.py
+++ scripts/pid_driver.py
@@ -8,8 +8,9 @@ def smart_logger(thestr):
     global last_log
     global count_log
     global left_wall_change
+    global delta_from_goal
     if (thestr != last_log):
-        print("%s %s %2.1f %2.1f %s" % (count_log, closest_dir, closest, left_wall_change, thestr))
+        print("#%s %s=%2.1f chgleft:%2.1f goaldif:%2.1f %s" % (count_log, closest_dir, closest, left_wall_change, delta_from_goal, thestr))
         count_log = count_log + 1
     last_log = thestr
 
@@ -41,38 +42,44 @@ detect_sub = rospy.Subscriber('detector', Detector, scan_callback)
 
 # Initialize this program as a node
 rospy.init_node('pid_demo')
-closest = 100
-closest_dir = "na"
+closest = 5
+closest_dir = "left"
 current_left = 0
 last_left = 0
-wall_dist = 0.7
+wall_dist = 0.4
 last_log = ""
 count_log = 0
 buffer = [0]*1000
 left_wall_change = 5
 tw_for = Twist()
-tw_for.linear.x = 0.3
+tw_for.linear.x = 0.1
 tw_rot = Twist()
 tw_rot.angular.z = -0.4
 tw_stop = Twist()
 
 # rate object gets a sleep() method which will sleep 1/10 seconds
-rate = rospy.Rate(5)
+rate = rospy.Rate(30)
 
 ### State
 
 while not rospy.is_shutdown():
     buffer.pop(1)
     buffer.append(abs(current_left-last_left))
-    delta_from_goal = wall_dist - closes
-    left_wall_change = abs(buffer[0]-buffer[-1])
-    if (closest_dir != "forward" and closest > wall_dist):
-        smart_logger("too far, turn towards wall")
+    delta_from_goal = wall_dist - closest
+    left_wall_change = buffer[0]-buffer[-1]
+    if (closest < 0.3):
+        smart_logger("Too close. Emergency Stop.")
+        cmd_vel_pub.publish(tw_stop)
+    elif (closest_dir != "forward" and delta_from_goal < -0.5):
+        smart_logger("Too far. Turn towards wall")
         cmd_vel_pub.publish(tw_rot)
-    elif(closest_dir == "forward" and closest > wall_dist):
-        smart_logger("too far. Drive towards wall")
+    elif(closest_dir == "forward" and delta_from_goal < -0.5):
+        smart_logger("Too far. Drive towards wall")
         cmd_vel_pub.publish(tw_for)
-    elif (closest_dir == "left" and delta_from_goal < 0.2):
+    elif (closest_dir != "left" and abs(delta_from_goal) <= 0.1):
+        smart_logger("near wall, turning towards it.")
+        cmd_vel_pub.publish(tw_rot)
+    elif (closest_dir == "left" and abs(delta_from_goal) <= 0.1):
         smart_logger("following wall")
         cmd_vel_pub.publish(tw_for)
     else:
