58db338802dc0f80cec65774b2cfaec1035204d4
Pito Salas <pitosalas@gmail.com>
2019-08-14 18:27:53 -0400

    making examples work

--- scripts/follower_line_finder.py
+++ scripts/follower_line_finder.py
@@ -6,21 +6,35 @@ from sensor_msgs.msg import Image
 class Follower:
     def __init__(self):
         self.bridge = cv_bridge.CvBridge()
-        cv2.namedWindow("window", 1)
         self.image_sub = rospy.Subscriber('camera/image', Image, self.image_callback)
 
     def image_callback(self, msg):
+
+        # get image from camera
         image = self.bridge.imgmsg_to_cv2(msg)
+
+        # filter out everything that's not yellow
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        #  HSV: Hue Saturation Value
-        # Trial and error found this to filter out just the yellow line
         lower_yellow = numpy.array([ 40, 0, 0])
         upper_yellow = numpy.array([ 120, 255, 255])
         mask = cv2.inRange(hsv,  lower_yellow, upper_yellow)
         masked = cv2.bitwise_and(image, image, mask=mask)
-        cv2.imshow("mask", mask)
-        cv2.imshow("masked", masked)
-        cv2.imshow("original", image)
+
+    # clear all but a 20 pixel band near the top of the image
+        h, w, d = image.shape
+        search_top = 3 * h /4
+        search_bot = search_top + 20
+        mask[0:search_top, 0:w] = 0
+        mask[search_bot:h, 0:w] = 0
+        cv2.imshow("band", mask)
+
+    # Compute the "centroid" and display a red circle to denote it
+        M = cv2.moments(mask)
+        if M['m00'] > 0:
+            cx = int(M['m10']/M['m00']) + 100
+            cy = int(M['m01']/M['m00'])
+            cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+        cv2.imshow("image", image)
         cv2.waitKey(3)
 
 rospy.init_node('follower')
---
+++ scripts/follower_p.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import rospy, cv2, cv_bridge, numpy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+
+class Follower:
+    def __init__(self):
+        self.bridge = cv_bridge.CvBridge()
+        self.image_sub = rospy.Subscriber('camera/image', Image, self.image_callback)
+        self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.twist = Twist()
+        self.logcount = 0
+        self.lostcount = 0
+
+    def image_callback(self, msg):
+
+        # get image from camera
+        image = self.bridge.imgmsg_to_cv2(msg)
+
+        # filter out everything that's not yellow
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower_yellow = numpy.array([ 40, 0, 0])
+        upper_yellow = numpy.array([ 120, 255, 255])
+        mask = cv2.inRange(hsv,  lower_yellow, upper_yellow)
+        masked = cv2.bitwise_and(image, image, mask=mask)
+
+    # clear all but a 20 pixel band near the top of the image
+        h, w, d = image.shape
+        search_top = 3 * h /4
+        search_bot = search_top + 20
+        mask[0:search_top, 0:w] = 0
+        mask[search_bot:h, 0:w] = 0
+        cv2.imshow("band", mask)
+
+    # Compute the "centroid" and display a red circle to denote it
+        M = cv2.moments(mask)
+        self.logcount += 1
+        print("M00 %d %d" % (M['m00'], self.logcount))
+
+        if M['m00'] > 0:
+            cx = int(M['m10']/M['m00']) + 100
+            cy = int(M['m01']/M['m00'])
+            cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+
+            # Move at 0.2 M/sec
+            # add a turn if the centroid is not in the center
+            # Hope for the best. Lots of failure modes.
+            err = cx - w/2
+            self.twist.linear.x = 0.2
+            self.twist.angular.z = -float(err) / 100
+            self.cmd_vel_pub.publish(self.twist)
+        cv2.imshow("image", image)
+        cv2.waitKey(3)
+
+rospy.init_node('follower')
+follower = Follower()
+rospy.spin()
