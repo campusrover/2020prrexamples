e5c4225abb6997dbe0f634a16a1fa33b3c1d9d3f
Pito Salas <pitosalas@gmail.com>
2019-12-04 13:27:24 -0500

    update and improved pid_range

--- scripts/pid_range.py
+++ scripts/pid_range.py
@@ -2,6 +2,7 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from prrexamples.msg import Detector
+import numpy as np
 
 # float32 narrow_l1
 # float32 narrow_l2
@@ -16,27 +17,42 @@ from prrexamples.msg import Detector
 # float32 closest_dist
 # string closest_dir
 
-
 def calc_range(range, mp1, mp2, wedge):
-    combo = range[mp1-wedge:mp1] + range[mp2:mp2+wedge]
-    mean = sum(combo) / len(combo)
+    r1 = [dist for dist in range[mp1-wedge:mp1] if dist != 0]
+    r2 = [dist for dist in range[mp2:mp2+wedge] if dist != 0]
+    combo = r1 + r2
+    if (len(combo) != 0):
+        mean = sum(combo) / len(combo)
+    else:
+        mean = float('nan')
     return mean
 
+def calc_range_1(range, start, length):
+    r1 = [dist for dist in range[start:start+length] if dist != 0]
+    if (len(r1) != 0):
+        mean = sum(r1) / len(r1)
+    else:
+        mean = float('nan')
+    return mean
 
 def scan_callback(msg):
     global counter
+
     forward = calc_range(msg.ranges, 359, 0, 15)
     right = calc_range(msg.ranges, 270, 271, 15)
     left = calc_range(msg.ranges, 90, 91, 15)
     back = calc_range(msg.ranges, 180, 181, 15)
-    narrow_l1 = sum(msg.ranges[83:87])/5
-    narrow_l2 = sum(msg.ranges[88:92])/5
-    narrow_l3 = sum(msg.ranges[93:97])/5
-    narrow_r1 = sum(msg.ranges[273:277])/5
-    narrow_r2 = sum(msg.ranges[268:272])/5
-    narrow_r3 = sum(msg.ranges[263:267])/5
-    closest_dist = min(narrow_l1, narrow_l2, narrow_l3, narrow_r1, narrow_r2, narrow_r3,
-                       forward, left, right, back)
+    narrow_l1 = sum(msg.ranges[83:87])
+
+    narrow_l1 = calc_range_1(msg.ranges, 83, 5)
+    narrow_l2 = calc_range_1(msg.ranges, 88, 5)
+    narrow_l3 = calc_range_1(msg.ranges, 93, 5)
+    narrow_r1 = calc_range_1(msg.ranges, 273, 5)
+    narrow_r2 = calc_range_1(msg.ranges, 268, 5)
+    narrow_r3 = calc_range_1(msg.ranges, 263, 5)
+
+    closest_dist = np.nanmin([narrow_l1, narrow_l2, narrow_l3, narrow_r1, narrow_r2, narrow_r3,
+                       forward, left, right, back])
     if (closest_dist == forward):
         closest_dir = "forward"
     elif (closest_dist == left):
@@ -60,32 +76,11 @@ def scan_callback(msg):
     else:
         closest_dir = "bug"
 
-
-# foos = {
-#     'forward': calc_range(msg.ranges, 359, 0, 15),
-#     'right': calc_range(msg.ranges, 270, 271, 15),
-#     'left': calc_range(msg.ranges, 90, 91, 15),
-#     'back': calc_range(msg.ranges, 180, 181, 15),
-#     'narrow_l1': sum(msg.ranges[83:87]/5),
-#     'narrow_l2': sum(msg.ranges[88:92])/5,
-#     'narrow_l3': sum(msg.ranges[93:97])/5,
-#     'narrow_r1': sum(msg.ranges[273:277])/5,
-#     'narrow_r2': sum(msg.ranges[268:272])/5,
-#     'narrow_r3': sum(msg.ranges[263:267])/5,
-# }
-
-# closest_dir = min(foos, key=foos.get)
-
     counter += 1
     if (counter % 2 == 0):
         d = Detector(narrow_l1, narrow_l2, narrow_l3, narrow_r1, narrow_r2, narrow_r3,
                     forward, left, right, back, closest_dist, closest_dir)
-        # rospy.loginfo("CLOSEST: %s at %1.1f flrb: %1.1f %1.1f %1.1f %1.1f "+
-        #             "nl: %1.2f %1.2f %1.2f, nr: %1.2f %1.2f %1.2f",
-        #             closest_dir, closest_dist, forward, left, right, back, narrow_l1, 
-        #             narrow_l2, narrow_l3, narrow_r1, narrow_r2, narrow_r3)
-
-        log_msg = "CLOSEST: %s at %1.1f flrb: %1.1f %1.1f %1.1f %1.1f nl: %1.2f %1.2f %1.2f, nr: %1.2f %1.2f %1.2f" % \
+        log_msg = "\n---\nCLOSEST: %s at %1.1f\nflrb: %1.1f %1.1f %1.1f %1.1f\nnl: %1.2f %1.2f %1.2f, nr: %1.2f %1.2f %1.2f" % \
                     (closest_dir, closest_dist, forward, left, right, back, narrow_l1, 
                     narrow_l2, narrow_l3, narrow_r1, narrow_r2, narrow_r3)
         rospy.loginfo_throttle(5, log_msg)
