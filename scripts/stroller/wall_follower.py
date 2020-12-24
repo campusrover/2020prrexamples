054d07974063a0fe0360bedf31d8da91e8871cf8
Pito Salas <pitosalas@gmail.com>
2020-12-24 17:38:21 +0000

    tuning

--- scripts/stroller/wall_follower.py
+++ scripts/stroller/wall_follower.py
@@ -5,12 +5,34 @@ import tf
 from std_msgs.msg import String, Header
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
-from math import sqrt, cos, sin, pi, atan2
+from math import sqrt, cos, sin, pi, atan2, isnan
 import numpy
 import sys
+from prrexamples.msg import Sensor
+from std_msgs.msg import ColorRGBA
+from math import pi, radians
+from marker_array_utils import MarkerArrayUtils
+
+GAZEBO=False
+FRONT_BEAR=0
+RIGHT_BEAR=90
+REAR_BEAR=180
+LEFT_BEAR=270
+
+green = ColorRGBA(0, 1, 0, 1)
+red = ColorRGBA(1, 0, 0, 1)
+grey = ColorRGBA(0, 0, 0, 1)
+
 
 from dynamic_reconfigure.server import Server
 from prrexamples.cfg import FollowerConfig
+
+def invert_angle(angle):
+    """To make the arrow markers work correctly we need to flip the angle because angles go the other way there"""
+    if (not GAZEBO):
+        return (angle + pi) % (2 * pi)
+    else:
+        return angle
 
 class PID:
     def __init__(self, Kp, Td, Ti, dt):
@@ -38,7 +60,7 @@ class PID:
 
         #Calculating the PID Control
         self.control = self.Kp * self.curr_error + self.Ti * self.sum_error + self.Td * self.curr_error_deriv
-        print self.Kp, self.Ti, self.Td, self.control, current_error
+        print "pid (%.1f,%.1f,%.1f) curr_err: %2f, control: %.2f" % (self.Kp, self.Ti, self.Td, self.curr_error, self.control)
 
     def get_control(self):
         return self.control
@@ -46,7 +68,9 @@ class PID:
 class WallFollower:
     def __init__(self):
         rospy.init_node('wall_follower', anonymous=True)
-        self.hz = 50 
+        self.hz = 20
+        self.distance_from_left_wall = 0
+        self.shutdown_requested = False
 
         #Setting the PID
         self.controller=PID(-1,-1.2, 0.00001,0.1)
@@ -66,22 +90,31 @@ class WallFollower:
         
         #Creating a Subscriber that will call laser_scan_callback every Laser Scan
         self.laser_sub = rospy.Subscriber("/scan", LaserScan, self.laser_scan_callback)
-        
+
+        self.sensor_sub = rospy.Subscriber('/sensor', Sensor,  self.sensor_callback)
+
+    def draw_markers(self, forward, left, right, rear, shortest_bearing, shortest):
+        mu = MarkerArrayUtils()
+        mu.add_marker(5, red, invert_angle(shortest_bearing), shortest)
+        mu.add_marker(2, grey, invert_angle(radians(LEFT_BEAR)), left)
+        mu.add_marker(3, green, invert_angle(radians(RIGHT_BEAR)), right)
+
+        mu.publish()
+
+    def sensor_callback(self, msg):
+        global target_bearing, target_distance
+        self.draw_markers(msg.forward, msg.left, msg.right, msg.rear, msg.shortest_bearing, msg.shortest)
+        if isnan(msg.left):
+            return
+        self.distance_from_left_wall = msg.left
+        print("left: %.2f dfl: %.2f ddfw: %.2f" % (msg.left, self.distance_from_left_wall, self.desired_distance_from_wall))
 
     def laser_scan_callback(self, msg):
-   
         cross_track_error=0
-        distance_from_wall=999999
         self.update_params()
 
-        #Finding the closest distance from the wall the robot reaches
-        #Divided by 2 so it goes to the left wall and not the one on the right
-       	for i in range(len(msg.ranges)/2):
-       		if msg.ranges[i] < distance_from_wall:
-       			distance_from_wall = msg.ranges[i]
-
        	#Calculating the cross track error
-       	cross_track_error = self.desired_distance_from_wall - distance_from_wall
+       	cross_track_error = self.desired_distance_from_wall - self.distance_from_left_wall
         
        	#Updating the controller and publishing the cross track error
        	self.controller.update_control(cross_track_error)
@@ -94,14 +127,16 @@ class WallFollower:
        	cmd.angular.z = self.controller.get_control()
        	
        	#Publishing the cmd.linear.x
+        print "lin: %.2f ang: %.2f" % (cmd.linear.x, cmd.angular.z)
        	self.cmd_pub.publish(cmd)
    
             
     def run(self):
         rate = rospy.Rate(self.hz)
-        while not rospy.is_shutdown():
+        while not rospy.is_shutdown() and not self.shutdown_requested:
             rate.sleep()
-
+        self.stop()
+        
     # Update latest params
     def update_params(self):
         self.forward_speed = rospy.get_param("~forward_speed")
@@ -111,7 +146,20 @@ class WallFollower:
     	self.controller.Ti = rospy.get_param("~Ti")
         self.controller.dt = rospy.get_param("~dt")
     
+    def stop(self):
+        twist = Twist()
+        twist.linear.x = 0
+        twist.angular.z = 0
+        self.cmd_pub.publish(twist)
+
+    def shutdown_hook(self):
+        self.shutdown_requested = True
+        print("\n**** Shutdown Requested ****")
+        self.stop()
+
+
 if __name__ == '__main__':
     wfh = WallFollower()
+    rospy.on_shutdown(wfh.shutdown_hook)
     wfh.run()
 
